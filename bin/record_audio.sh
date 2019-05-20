88e14d08aae9152cd0a7c54208ab4cbc9ad3fa46
Uwe Gerdes <entwicklung@uwegerdes.de>
2019-05-20 21:08:13 +0200

    change capture filenames

--- bin/record_audio.sh
+++ bin/record_audio.sh
@@ -2,7 +2,7 @@
 
 if [ -f "/dev/snd/pcm*c" ]; then
 	if [ "$1" == "start" ] ; then
-		arecord -D default:CARD=Device --fatal-errors --buffer-size=192000 -f dat -t raw | dd bs=240000 | lame -r --preset standard - /home/node/app/capture/%v-%Y%m%d%H%M%S.mp3
+		arecord -D default:CARD=Device --fatal-errors --buffer-size=192000 -f dat -t raw | dd bs=240000 | lame -r --preset standard - /home/node/app/capture/%Y%m%d%H%M%S-%v.mp3
 	elif [ "$1" == "stop" ] ; then
 		pkill -6 arecord
 	else
