a8141f21773a85d58049c09affabd6ecb1f68b59
Uwe Gerdes <entwicklung@uwegerdes.de>
2019-07-12 21:53:15 +0200

    check for selfsigned.key

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ if [ ! -w "${APP_HOME}/" ]; then
 	exit 1
 fi
 
-if [ ! -d "${APP_HOME}/key" ]; then
-	mkdir "${APP_HOME}/key"
+if [ ! -f "${APP_HOME}/key/selfsigned.key" ]; then
+	mkdir -p "${APP_HOME}/key"
 	# see https://www.digitalocean.com/community/tutorials/openssl-essentials-working-with-ssl-certificates-private-keys-and-csrs
 	openssl req -new -newkey rsa:2048 -nodes -x509 -days 3650 -out "${APP_HOME}/key/selfsigned.crt" -keyout "${APP_HOME}/key/selfsigned.key"
 	# -subj "/C=US/ST=New York/L=Brooklyn/O=Example Company/CN=example.com"
